f14b7b4c0f104c60d05cec26a9adc7a374650e79
Andres <hewsanchez@gmail.com>
2022-03-30 17:49:48 -0400

    Frist Commit

---
+++ scripts/googleapis.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:10.161055 ,lng: -67.9953359};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
